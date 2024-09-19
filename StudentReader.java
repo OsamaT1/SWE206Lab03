3cd3f21d71cdb3c027e15a35d08ccff2162c100e
Abdulaziz <ahrhq2004@gmail.com>
2024-09-19 21:38:57 +0300

    Device2 Changes

--- StudentReader.java
+++ StudentReader.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class StudentReader {
-    
+    public static void main (String[] args) {
+        System.out.println("Print the list of students");
+        createRandom();
+    }
+    public static void createRandom() {
+        System.out.println("Create the list of students");
+    }
+
+        public void method(ArrayList<String> list){
+
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+    }
+}
+
+public ArrayList<String> createRandomStudents(){
+    ArrayList<String> arrayList = new ArrayList<>();
+    arrayList.add("Osama");
+    arrayList.add("tarek");
+    arrayList.add("Abdulaziz");
+    return arrayList;
+}
+
 }
